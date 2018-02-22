e08d2972e3f7632086179d77919d7365d032ef2c
Amanda <teo.lt.amanda@gmail.com>
2018-02-22 19:20:28 +0000

    Added additional graph
    Distribution of number of rounds per match

--- Counterstrike analysis.R
+++ Counterstrike analysis.R
@@ -2,13 +2,13 @@
 #
 #
 # Description: Analyse counterstrike data
-# Project: Introduction to R
+# Project: R training
 #
 #
 ##############################################################################
 
 ### set paths
-datapath <- "/Users/amanda/Desktop/Counterstrike"
+datapath <- "C:/Amanda.Teo/Desktop/Coding for beginners/Data"
 
 library(ggplot2)
 
@@ -20,15 +20,15 @@ str(df)
 
 #############################################################################
 #
-# Rudimentary analysis
+# Rudimentary stats
 #
 #############################################################################
 
 # How to view subsets of data
-View(df[, c("att_team", "winner_team")]) 
+View(df[, c("file", "round", "att_team", "winner_team")]) 
 
 # How to view counts of of a certain variable
-table(df[, c("winner_team")]) # winner_team is winning team 
+table(df$winner_team) # winner_team is winning team 
 
 # file is unique to each match - but very cumbersome - create new id
 head(df$file)
@@ -37,38 +37,54 @@ df$file_id <- as.numeric((df$file)) #can use interaction for more than 1 group
 #number of matches
 max(df$file_id) #1297
 
-#average number of rounds within a match
-num_rounds <- aggregate(round ~ file_id, df, FUN = max)
-nrow(num_rounds)
-mean(num_rounds$round) #26 rounds per match
-
-#total number of games(rounds) played
-sum(num_rounds$round) #32752
-
 #number of teams
 NROW(unique(c(df$att_team, df$vic_team))) #27
 
 #number of players
 NROW(unique(c(df$att_id, df$vic_id))) #11,130 unique players
 
+#average number of rounds within a match
+num_rounds <- aggregate(round ~ file_id, df, FUN = max)
+nrow(num_rounds)
+mean(num_rounds$round) #26 rounds per match
+
+#distribution of rounds per match
+summary(num_rounds$round)
+
+#total number of games(rounds) played
+sum(num_rounds$round) #32752
+
+#############################################################################
+#
+# Data cleaning
+#
+#############################################################################
+
 #any missing values
 num_miss <- lapply(df,FUN = function(x) {
-                          length(which(is.na(x)))
-                  })
+  length(which(is.na(x)))
+})
 
 #translate in game coordinates into positives
 df[,grep("_pos_[xy]", colnames(df))] <- sapply(df[,grep("_pos_[xy]", colnames(df))], FUN = function(x) {
-                                                                                        x + 10000  
+  x + 10000  
 })
 
 #same winning team within a round of a match
 winners_temp <- aggregate(winner_team ~ file_id + round, df,FUN = function (x) {
-                                                                length(unique(x))
-                                                        })
+  length(unique(x))
+})
 winners_temp[winners_temp$winner_team > 1, ] #check how many rounds per match have more than 1 winner
 rm(winners_temp)
 
-#most popular weapons
+#############################################################################
+#
+# More intricate summary statistics
+#
+#############################################################################
+
+#within each weapon type, percentage of hits on different parts of the body
+
 df$counter <- 1
 
 weapons <- df[, c("hitbox", "wp_type", "counter")]
@@ -78,18 +94,19 @@ weapons <- with(weapons, weapons[order(wp_type, hitbox),])
 weapons$perc_freq <- with(weapons, counter/total_use_wptype) * 100
 
 weapons_table <- reshape(weapons, idvar = c("wp_type"), drop = c("counter", "total_use_wptype"), timevar = c("hitbox"), direction = "wide")
-#seems that likelihood of body part hit not really dependent on the weapon (which makes sense)
+View(weapons_table)#seems that likelihood of body part hit not really dependent on the weapon (which makes sense)
 df$counter <- NULL
 
 #top 15 players by average damge per attack, the number of attacks, favourite weapon
+
 indiv_damage <- data.frame(
-                  aggregate(hp_dmg ~ att_id, df,FUN = sum),
-                  aggregate(wp ~ att_id, df, FUN = function(x) {
-                        ux <- unique(x)
-                        ux[which.max(tabulate(match(x, ux)))]
-                      }),
-                  aggregate(X ~ att_id, df,FUN = length)
-                  )
+  aggregate(hp_dmg ~ att_id, df,FUN = sum),
+  aggregate(wp ~ att_id, df, FUN = function(x) {
+    ux <- unique(x)
+    ux[which.max(tabulate(match(x, ux)))]
+  }),
+  aggregate(X ~ att_id, df,FUN = length)
+)
 indiv_damage[, grep("att_id.[0-9]", colnames(indiv_damage))] <- list(NULL)
 indiv_damage <- indiv_damage[!indiv_damage$att_id == 0,]
 
@@ -99,6 +116,9 @@ top15_damage <- indiv_damage[1:15,]
 
 #distribution of average damage
 ggplot(data = indiv_damage, aes(x = avg_dmg)) + geom_histogram(binwidth = 5)
+
+indiv_damage$top_15 <- indiv_damage$att_id %in% top15_damage$att_id
+ggplot(data = indiv_damage, aes(x = avg_dmg)) + geom_histogram(binwidth = 5, aes(fill = top_15))
 
 #############################################################################
 #
@@ -121,9 +141,6 @@ ggplot(data = df[df$game_id == 1, ]) + geom_bar(stat="count", aes(map)) + coord_
 # which maps benefit one side more than the other?
 game_winners <- df[df$game_id == 1, c("winner_side", "map")]
 
-ggplot(data = game_winners, aes(map,..count..)) + 
-      geom_bar(aes(fill = winner_side), position = "Dodge") +
-      coord_flip()
-  
-
-
+ggplot(data = game_winners, aes(map)) + 
+  geom_bar(stat = "count", aes(fill = winner_side), position = "Dodge") +
+  coord_flip()
